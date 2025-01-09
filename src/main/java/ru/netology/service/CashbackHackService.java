b22f2060015384fe5bd052aa9fcaa54bba2352b3
O.Gogol <o.gogol91@gmail.com>
2025-01-09 18:07:34 +0000

    removed wrong code

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,9 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount % boundary == 0) {
-            return 0;
-        }
         return boundary - amount % boundary;
     }
 }
